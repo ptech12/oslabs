a706c4043fe964eef29c4ee669e3ce1700e2dc39
Prasanth A R <raviprasanth45@gmail.com>
2021-06-02 08:34:54 +0530

    Update fdisk.c

--- fdisk.c
+++ fdisk.c
@@ -1,14 +1,13 @@
 #include<stdio.h> 
-main() 
+void main() 
 { 
-int t[20], n, I, j, tohm[20], tot=0; 
+int t[20], n, i, j, tohm[20], tot=0; 
 float avhm; 
-clrscr(); 
-printf(“enter the no.of tracks”); 
-scanf(“%d”,&n); 
-printf(“enter the tracks to be traversed”); 
+printf("enter the no.of tracks"); 
+scanf("%d",&n); 
+printf("enter the tracks to be traversed"); 
 for(i=2;i<n+2;i++) 
-scanf(“%d”,&t*i+); 
+scanf("%d",&t[i]); 
 for(i=1;i<n+1;i++) 
 { 
 tohm[i]=t[i+1]-t[i]; 
@@ -18,8 +17,7 @@ tohm[i]=tohm[i]*(-1);
 for(i=1;i<n+1;i++) 
 tot+=tohm[i]; 
 avhm=(float)tot/n; 
-printf(“Tracks traversed\tDifference between tracks\n”); for(i=1;i<n+1;i++) 
-printf(“%d\t\t\t%d\n”,t*i+,tohm*i+); 
+printf("Tracks traversed\tDifference between tracks\n"); for(i=1;i<n+1;i++) 
+printf("%d\t\t\t%d\n",t[i],tohm[i]); 
 printf("\nAverage header movements:%f",avhm); 
-getch(); 
 } 
