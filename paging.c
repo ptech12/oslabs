2c55ae364be97c4ad5ab0309c62278daeeb8911d
Prasanth A R <raviprasanth45@gmail.com>
2021-06-02 08:26:10 +0530

    FDISK algorithm

--- paging.c
+++ paging.c
@@ -4,7 +4,6 @@ void main()
 { 
     int ms, ps, nop, np, rempages, i, j, x, y, pa, offset; 
     int s[10], fno[10][20]; 
-    clrscr(); 
     printf("\nEnter the memory size -- "); 
     scanf("%d",&ms); 
     printf("\nEnter the page size -- "); 
